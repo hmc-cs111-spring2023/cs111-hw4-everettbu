bc57e485d72fc688697001f0a49ea0d8ea0a50e4
Everett Butler <64377136+everettbu@users.noreply.github.com>
2023-02-12 21:11:11 +0000

    working

--- src/main/scala/collatz/Collatz.scala
+++ src/main/scala/collatz/Collatz.scala
@@ -1,1 +1,5 @@
+def collatz(x: Int): Int = {
+  if (x % 2 == 0) x / 2
+  else 3 * x + 1
+}
 
--- src/test/scala/collatz/CollatzSpec.scala
+++ src/test/scala/collatz/CollatzSpec.scala
@@ -3,6 +3,28 @@ import org.scalatest.matchers.should._
 
 class CollatzSpec extends AnyFunSuite with Matchers:
 
-// write tests here!
+  test("0!") {
+   collatz(6) should be (3)
+  }
+
+  test("1!") {
+    collatz(7) should be (22)
+  }
+
+  test("2!") {
+    collatz(1) should be (4)
+  }
+
+  test("3!") {
+    collatz(2) should be (1)
+  }
+
+  test("4!") {
+    collatz(42) should be (21)
+  }
+
+  test("5!") {
+    collatz(101) should be (304)
+  }
 
 end CollatzSpec
